894cab40d59c4b02c83695592522311ee2b4b177
Asmaa Adel Alamir <114780478+asmaaadel39@users.noreply.github.com>
2024-07-24 16:38:01 +0300

    Add files via upload

---
+++ tic_tac.py
@@ -0,0 +1,82 @@
+#STEP 1
+#The following line initializes a list called board with 9 spaces, 
+# representing the 9 spaces on the Tic Tac Toe board.
+board = [" " for x in range(9)]
+
+
+#STEP 2
+#The following function print_board() will print the Tic Tac Toe board.
+#It creates 3 strings, row1, row2, and row3, to represent each row of the board.
+
+def print_board():
+    row1 = "| {} | {} | {} |".format(board[0], board[1], board[2])
+    row2 = "| {} | {} | {} |".format(board[3], board[4], board[5])
+    row3 = "| {} | {} | {} |".format(board[6], board[7], board[8])
+
+
+    print()
+    print(row1)
+    print(row2)
+    print(row3)
+    print()
+
+#print_board()
+
+
+#STEP 3 The Player move 
+def player_move(icon):
+    if icon == "X":
+        number = 1
+    elif icon == "O":
+        number = 2
+    print("Your turn player {}".format(number))
+    choice = int(input("Enter your move (1-9): ").strip())
+    if board[choice - 1] == " ":
+        board[choice - 1] = icon
+    else:
+        print()
+        print("That space is already taken!")
+
+#STEP 4 check whos win 
+
+def is_victory(icon):
+    if (board[0] == icon and board[1] == icon and board[2] == icon) or \
+       (board[3] == icon and board[4] == icon and board[5] == icon) or \
+       (board[6] == icon and board[7] == icon and board[8] == icon) or \
+       (board[0] == icon and board[3] == icon and board[6] == icon) or \
+       (board[1] == icon and board[4] == icon and board[7] == icon) or \
+       (board[2] == icon and board[5] == icon and board[8] == icon) or \
+       (board[0] == icon and board[4] == icon and board[8] == icon) or \
+       (board[2] == icon and board[4] == icon and board[6] == icon):
+       return True
+    else:
+        return False
+
+# STEP 5 check if draw
+
+def is_draw():
+    if " " not in board:
+        return True
+    else:
+        return False
+    
+# STEP 6 Check all conditions 
+
+while True:
+    print_board()
+    player_move("X")
+    print_board()
+    if is_victory("X"):
+        print("X wins! Congratulations!")
+        break
+    elif is_draw():
+        print("It's a draw!")
+        break
+    player_move("O")
+    if is_victory("O"):
+        print_board()
+        print("O wins! Congratulations!")
+        break
+    elif is_draw():
+        print("It's a draw!")
+        break
